07d38141d90a19bc22db4c367f57a466e80281d4
unknown <222vikassingh@gmail.com>
2024-11-03 23:52:42 +0530

    second commit removed the @SpringBootTest Annotation

--- src/test/java/com/javahub/CicddemoApplicationTests.java
+++ src/test/java/com/javahub/CicddemoApplicationTests.java
@@ -10,7 +10,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@SpringBootTest
 @WebMvcTest
 class CicddemoApplicationTests {
 
